e8aab2cf49bd0347908506c1f0b4351e586aaf78
anshul9809 <kumar.anshul9809@gmail.com>
2024-06-24 15:34:54 +0530

    Starting with the dashboard

--- dashboard/src/pages/Login/Login.jsx
+++ dashboard/src/pages/Login/Login.jsx
@@ -1,15 +1,26 @@
-import { useState } from "react";
-import { toast } from "react-toastify";
 import styles from "./login.module.scss";
-
+import {Link} from "react-router-dom"; 
 const Login = ()=>{
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    return <>
-        <div className={styles.mainDiv}>
-            <h1>Login</h1>
+
+    return (
+        <div className={styles.login}>
+            <div className={styles.container}>
+                <h1>Login</h1>
+                <div className={styles.input}>
+                    <div className={styles.label}>Email</div>
+                    <input className={styles.inputField} type="text" placeholder="Email"/>
+                </div>
+                <div className={styles.input}>
+                    <div className={styles.label}>Password</div>
+                    <input className={styles.inputField} type="password" placeholder="Password"/>
+                </div>
+                <button className={styles.button}>Login</button>
+                <Link to="/password/forgot" className={styles.forgot}>
+                    Forgot Password?
+                </Link>
+            </div>
         </div>
-    </>
+    )
 }
 
 export default Login;
