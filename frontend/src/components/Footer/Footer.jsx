02d31f209085aef7cec65ca87a580fe44b641f8a
anshul9809 <kumar.anshul9809@gmail.com>
2024-07-16 17:34:07 +0530

    Completed the responsiveness for header and footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
 import styles from "./footer.module.scss";
+import { FaEnvelope, FaFacebook, FaGithub, FaInstagram, FaLinkedinIn, FaPhone, FaTwitter } from "react-icons/fa6";
 const Footer = ()=>{
     const {user} = useSelector(state=>state.user);
 
@@ -9,13 +10,13 @@ const Footer = ()=>{
         <footer className={styles.footer}>
             <div className={styles.footer__wrapper}>
                 <div className={styles.footer__social__links}>
-                    {user.phone? <a href={`tel:${user.phone}`} target="_blank" rel="noreferrer" className={styles.footer__social__link}><img src="/phone.png" alt="phone" /></a>: null}
-                    {user.email?<a href={`mailto:${user.email}`} target="_blank" rel="noreferrer" className={styles.footer__social__link}><img src="/email.png" alt="mail" /></a>:null                    }
-                    {user.githubURL?<a href={user.githubURL} target="_blank" rel="noreferrer" className={styles.footer__social__link}><img src="/github.png" alt="github" /></a>:null}
-                    {user.linkedInURL?<a href={user.linkedInURL} target="_blank" rel="noreferrer" className={styles.footer__social__link}><img src="/linkedin.png" alt="linkedin" /></a>:null}
-                    {user.instagramURL?<a href={user.instagramURL} target="_blank" rel="noreferrer" className={styles.footer__social__link}><img src="/instagram.png" alt="instagram" /></a>:null}
-                    {user.twitterURL?<a href={user.twitterURL} target="_blank" rel="noreferrer" className={styles.footer__social__link}><img src="/twitter.png" alt="twitter" /></a>:null}
-                    {user.facebookURL?<a href={user.facebookURL} target="_blank" rel="noreferrer" className={styles.footer__social__link}><img src="/facebook.png" alt="facebook" /></a>:null}
+                    {user.phone? <a href={`tel:${user.phone}`} target="_blank" rel="noreferrer" > <FaPhone className={styles.icon} /> </a> : null}
+                    {user.email?<a href={`mailto:${user.email}`} target="_blank" rel="noreferrer" ><FaEnvelope className={styles.icon} /></a>:null                    }
+                    {user.githubURL?<a href={user.githubURL} target="_blank" rel="noreferrer" ><FaGithub className={styles.icon} /></a>:null}
+                    {user.linkedInURL?<a href={user.linkedInURL} target="_blank" rel="noreferrer" ><FaLinkedinIn className={styles.icon} /></a>:null}
+                    {user.instagramURL?<a href={user.instagramURL} target="_blank" rel="noreferrer" ><FaInstagram className={styles.icon} /></a>:null}
+                    {user.twitterURL?<a href={user.twitterURL} target="_blank" rel="noreferrer" ><FaTwitter className={styles.icon} /></a>:null}
+                    {user.facebookURL?<a href={user.facebookURL} target="_blank" rel="noreferrer" ><FaFacebook className={styles.icon} /></a>:null}
                 </div>
                 <p className={styles.footer__copyright}>Copyright &copy; {currentYear} . All rights reserved.</p>
                 <p className={styles.footer__madeby}>Made with ❤️ by Anshul</p>
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,6 +3,8 @@ import Sidebar from "../Sidebar/Sidebar";
 import styles from "./navbar.module.scss";
 import {motion} from "framer-motion";
 import { Link } from "react-router-dom";
+import { FaFacebook, FaGithub, FaInstagram, FaLinkedinIn, FaPhone, FaSquareArrowUpRight, FaEnvelope, FaTwitter } from "react-icons/fa6";
+
 const Navbar = ()=>{
     const {user} = useSelector(state=>state.user);
     
@@ -20,13 +22,13 @@ const Navbar = ()=>{
                     <Link to="/"><i>AK</i></Link>
                 </motion.span>
                     <div className={styles.navbar__social__links}>
-                        {user.phone? <a href={`tel:${user.phone}`} target="_blank" rel="noreferrer" ><img src="/phone.png" alt="phone" loading="lazy" /></a>: null}
-                        {user.email?<a href={`mailto:${user.email}`} target="_blank" rel="noreferrer" ><img src="/email.png" alt="mail" loading="lazy" /></a>:null                    }
-                        {user.githubURL?<a href={user.githubURL} target="_blank" rel="noreferrer" ><img src="/github.png" alt="github" loading="lazy" /></a>:null}
-                        {user.linkedInURL?<a href={user.linkedInURL} target="_blank" rel="noreferrer" ><img src="/linkedin.png" alt="linkedin" loading="lazy" /></a>:null}
-                        {user.instagramURL?<a href={user.instagramURL} target="_blank" rel="noreferrer" ><img src="/instagram.png" alt="instagram" loading="lazy" /></a>:null}
-                        {user.twitterURL?<a href={user.twitterURL} target="_blank" rel="noreferrer" ><img src="/twitter.png" alt="twitter" loading="lazy" /></a>:null}
-                        {user.facebookURL?<a href={user.facebookURL} target="_blank" rel="noreferrer" ><img src="/facebook.png" alt="facebook" loading="lazy" /></a>:null}
+                        {user.phone? <a href={`tel:${user.phone}`} target="_blank" rel="noreferrer" > <FaPhone className={styles.icon} /> </a> : null}
+                        {user.email?<a href={`mailto:${user.email}`} target="_blank" rel="noreferrer" ><FaEnvelope className={styles.icon} /></a>:null                    }
+                        {user.githubURL?<a href={user.githubURL} target="_blank" rel="noreferrer" ><FaGithub className={styles.icon} /></a>:null}
+                        {user.linkedInURL?<a href={user.linkedInURL} target="_blank" rel="noreferrer" ><FaLinkedinIn className={styles.icon} /></a>:null}
+                        {user.instagramURL?<a href={user.instagramURL} target="_blank" rel="noreferrer" ><FaInstagram className={styles.icon} /></a>:null}
+                        {user.twitterURL?<a href={user.twitterURL} target="_blank" rel="noreferrer" ><FaTwitter className={styles.icon} /></a>:null}
+                        {user.facebookURL?<a href={user.facebookURL} target="_blank" rel="noreferrer" ><FaFacebook className={styles.icon} /></a>:null}
                     </div>
                 </div>
             </div>
